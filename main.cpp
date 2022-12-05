b5603e2d49052f2205908d7ffa87904596b147ae
Mariana Molano <92944624+marianacmolano@users.noreply.github.com>
2022-12-05 17:50:57 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,46 @@
 #include "Applicant.h";
 using namespace std;
 
+// Function calculates applicant score
+float calcluateScore(int degs, float gpa, int experience, int diverse, int letter, int time, int interview);
+
+// Function Reads in data
+manager ReadFile(string file);
+
+int main() { 
+	
+    string fileName;
+    int choice;
+
+    cout << setw() << "Candidate Selection Database" << endl;
+    cout << "Select applicant dataset (.csv): " << endl;
+    cin >> fileName;
+
+    manager manage = ReadFile(fileName);
+    manage.buildBPlusTree();
+    manage.buildRedBlackTree();
+    cout << "Menu" << endl;
+    cout << "1. See individual applicant score" << endl;
+    cout << "2 Generate applicant score sheet" << endl;
+    cout << "3. Display top 50 applicants" << endl;
+
+    cin >> choice;
+
+    if (choice == 1) {  // See individual applicant score
+        string in;
+        cout << "Input applicant ID: " << endl;
+        cin >> in;
+        int find = stoi(in);
+    }
+    else if (choice == 2) { // Generate applicant score sheet
+
+
+    }
+    else if (choice == 3) // display top 50
+
+
+}
+
 float calcluateScore(int degs, float gpa, int experience, int diverse, int letter, int time, int interview) {
     float score;
     score = experience*20 + gpa*5 + time*5 + degs*20 + letter*10 + interview*25 + diverse*15;
@@ -54,16 +94,17 @@ manager ReadFile(string file) {
         experience = stoi(temp);
 
         getline(stream, temp, ',');
-        diverse = stoi(temp);
-
-        getline(stream, temp, ',');
         letter = stoi(temp);
 
         getline(stream, temp, ',');
         time = stoi(temp);
-
+	
         getline(stream, temp, ',');
         interview = stoi(temp);
+	
+	getline(stream, temp, ',');
+        diverse = stoi(temp);
+
 
         int score = calcluateScore(degs, gpa, experience, diverse, letter, time, interview);
 
@@ -75,42 +116,3 @@ manager ReadFile(string file) {
     }
     return manage;
 }
-
-int main() {
-
-// Print Input
-    string dataSet;
-    int choice;
-
-    cout << setw() << "Candidate Selection Database" << endl;
-    cout << "Select applicant dataset (.csv): " << endl;
-    cin >> dataSet;
-
-    manager manage = ReadFile(dataSet);
-    manage.buildBPlusTree();
-    manage.buildRedBlackTree();
-    cout << "Menu" << endl;
-    cout << "1. See individual applicant score" << endl;
-    cout << "2 Generate applicant score sheet" << endl;
-    cout << "3. Display top 50 applicants" << endl;
-
-    cin >> choice;
-
-    if (choice == 1) {  // See individual applicant score
-        string in;
-        cout << "Input applicant ID: " << endl;
-        cin >> in;
-        int find = stoi(in);
-    }
-    else if (choice == 2) { // Generate applicant score sheet
-
-
-    }
-    else if (choice == 3) // display top 50
-    //skThree(legos);
-// LOAD IN APPLICANT VALUES INTO TREE
-
-
-
-}
-

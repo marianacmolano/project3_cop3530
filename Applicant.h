e659de6b89837ff999f092bb2a14506d12204ee8
Mariana Molano <92944624+marianacmolano@users.noreply.github.com>
2022-12-06 10:30:35 -0500

    Update Applicant.h

--- Applicant.h
+++ Applicant.h
@@ -1,40 +1,22 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
 using namespace std;
 
 // Applicant struct- Creates nodes for Trees
-struct Applicant {
-    string applName;
-    Applicant* left;
-    Applicant* right;
-    Applicant* parent;
-    float score;
+struct Applicant{
     int applicantID;
+    float score;
 
 
     Applicant() { // Default constructor
-        score = 0;
         applicantID = 00000000; // 8 digit ID num
-        left = nullptr;
-        right = nullptr;
-        parent = nullptr;
+        score = 0.0;
     }
 
     Applicant(int id, float scr) {
         applicantID = id;
         score = scr;
-        left = nullptr;
-        right = nullptr;
-        parent = nullptr;
-    }
-
-    int getScore(Applicant* node) {
-        if(node == nullptr)
-            return 0;
-        else {
-            //return score
-            return node->score;
-        }
     }
 };
